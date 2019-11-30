95a4a7c1b27eed1e84a430fbabb0abb0b9290409
Viktor Braun (v_b) <mail@viktor-braun.de>
2019-11-30 15:47:05 +0100

    app layout

--- catcher/ContentView.swift
+++ catcher/ContentView.swift
@@ -2,7 +2,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { geometry in
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("\(10)")
+                        .foregroundColor(Color.blue)
+                        .font(.largeTitle)
+                    Spacer()
+                    
+                }
+                .edgesIgnoringSafeArea(.all)
+                .offset(x: 0, y: 60)
+                
+                ZStack{
+                    // food
+                    Circle()
+                        .frame(width: 5 * 2, height: 5 * 2)
+                        .foregroundColor(Color.blue)
+                        .position(x: 200, y: 200)
+                    
+                    // bomb
+                    Rectangle()
+                        .frame(width: 5 * 2, height: 5 * 2)
+                        .foregroundColor(Color.red)
+                        .position(x: 100, y: 100)
+                    
+                    // player
+                    Circle()
+                        .frame(width: 20 * 2, height: 20 * 2)
+                        .foregroundColor(Color.blue)
+                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                    
+                }
+                .padding()
+                .edgesIgnoringSafeArea(.all)
+                
+            }
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all)
+            
+        }
+        
     }
 }
 
