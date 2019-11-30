92f8c341ecbf7b8d82db82a603be3497e72b36d8
Viktor Braun (v_b) <mail@viktor-braun.de>
2019-11-30 16:12:05 +0100

    impl data structures

--- catcher/ContentView.swift
+++ catcher/ContentView.swift
@@ -1,6 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var player = Player()
+    
+    @State var food = [Item]()
+    @State var bomb = [Item]()
+    
+    
     var body: some View {
         GeometryReader { geometry in
             VStack{
@@ -17,16 +24,20 @@ struct ContentView: View {
                 
                 ZStack{
                     // food
-                    Circle()
-                        .frame(width: 5 * 2, height: 5 * 2)
-                        .foregroundColor(Color.blue)
-                        .position(x: 200, y: 200)
+                    ForEach(self.food, id: \.id) { item in
+                        Circle()
+                            .frame(width: item.radius * 2, height: item.radius * 2)
+                            .foregroundColor(Color.blue)
+                            .position(item.pos)
+                    }
                     
                     // bomb
-                    Rectangle()
-                        .frame(width: 5 * 2, height: 5 * 2)
-                        .foregroundColor(Color.red)
-                        .position(x: 100, y: 100)
+                    ForEach(self.bomb, id: \.id) { item in
+                        Rectangle()
+                            .frame(width: item.radius * 2, height: item.radius * 2)
+                            .foregroundColor(Color.red)
+                            .position(item.pos)
+                    }
                     
                     // player
                     Circle()
@@ -37,15 +48,52 @@ struct ContentView: View {
                 }
                 .padding()
                 .edgesIgnoringSafeArea(.all)
-                
             }
             .background(Color.black)
             .edgesIgnoringSafeArea(.all)
-            
+            .onAppear{
+                self.player.pos = CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                self.player.target = self.player.pos
+                
+                for _ in 1...3{
+                    self.food.append(.spawn(within: geometry))
+                }
+                
+                for _ in 1...5{
+                    self.bomb.append(.spawn(within: geometry))
+                }
+            }
         }
-        
     }
 }
+
+struct Player : GameElement{
+    var pos = CGPoint(x:0,y:0)
+    var target = CGPoint(x:0,y:0)
+    var radius = CGFloat(20.0)
+    
+}
+struct Item : GameElement, Equatable{
+    var id = UUID()
+    var pos = CGPoint(x:0,y:0)
+    var radius = CGFloat(5.0)
+    
+    static func spawn(within : GeometryProxy) -> Item{
+        var f = Item()
+        f.pos.x = CGFloat.random(in: 0..<within.size.width)
+        f.pos.y = CGFloat.random(in: 0..<within.size.height)
+        return f
+    }
+    
+}
+
+protocol GameElement  {
+    var pos: CGPoint { get }
+    var radius: CGFloat { get }
+    
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
